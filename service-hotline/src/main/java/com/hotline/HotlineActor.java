d0fcc00e34fd8b039cbf55fb46cf1fdd1bce6ebd
LegrosGabriel <133345446+LegrosGabriel@users.noreply.github.com>
2025-12-26 12:10:29 +0100

    Add logging for hotline call analysis

--- service-hotline/src/main/java/com/hotline/HotlineActor.java
+++ service-hotline/src/main/java/com/hotline/HotlineActor.java
@@ -3,6 +3,7 @@ package com.hotline;
 import com.framework.Actor;
 import com.framework.ActorMessage;
 import com.framework.ActorSystem;
+import com.framework.GestionLog;
 
 public class HotlineActor extends Actor {
     private final ActorSystem system;
@@ -20,23 +21,26 @@ public class HotlineActor extends Actor {
         String sender = message.getSenderId();
 
         System.out.println("• 📞 Hotline analyse l'appel de " + sender);
+        GestionLog.ecrire("HOTLINE", "Appel reçu de " + sender + " contenant : \"" + texte + "\"");
 
         if (texte.contains("thoracique") || texte.contains("bras gauche")) {
             // CAS 1 : C'EST GRAVE -> On envoie au Médecin Urgentiste avec priorité
             System.out.println("• \u001B[31m CODE ROUGE : Suspections infarctus !\u001B[0m");
+            GestionLog.ecrire("HOTLINE", "Analyse : CODE ROUGE (Infarctus) -> Transfert immédiat au Médecin");
             ActorMessage urgence = new ActorMessage(getId(), "medecin-urgences", "service-hopital", "INFARCTUS suspecté pour " + sender);
             system.send(urgence);
 
         } else if (texte.contains("épilepsie")) {
-            // CAS 2 : NEURO -> On pourrait avoir un médecin spécialisé (ou le même avec info différente)
+            // CAS 2 : NEURO -> On pourrait avoir un médecin spécialisé
             System.out.println("• \u001B[33m CODE ORANGE : Crise convulsive.\u001B[0m");
+            GestionLog.ecrire("HOTLINE", "Analyse : CODE ORANGE (Neuro) -> Transfert au Médecin");
             ActorMessage neuro = new ActorMessage(getId(), "medecin-urgences", "service-hopital", "NEURO : Crise épilepsie pour " + sender);
             system.send(neuro);
 
         } else if (texte.contains("jambe") || texte.contains("cassée")) {
             // CAS 3 : TRAUMA -> Pas besoin de réveiller le médecin urgentiste, on conseille juste
             System.out.println("• \u001B[32m CODE VERT : Trauma membre inférieur.\u001B[0m");
-
+            GestionLog.ecrire("HOTLINE", "Analyse : CODE VERT (Bobologie) -> Conseil direct au patient (Pas de médecin)");
             try { Thread.sleep(2000); } catch (InterruptedException e) {}
 
             // Hotline répond directement au lieu de transférer
@@ -45,6 +49,7 @@ public class HotlineActor extends Actor {
 
         } else {
             System.out.println("• ❓ Incompris, demande de précision.");
+            GestionLog.ecrire("HOTLINE", "Analyse : Message incompris, demande de précisions.");
         }
     }
 
